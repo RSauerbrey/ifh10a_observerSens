afb06631c0ed499b6246a64cc4da2cc710b6305a
RSauerbrey <113389368+RSauerbrey@users.noreply.github.com>
2024-05-06 17:59:17 +0200

    main programmiert

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include "sensor.h"
+#include "dashboard.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    TemperaturSensor* t = new TemperaturSensor;
+    
+    Dashboard* d[10];
+    for( int i=0; i<10; i++)
+    {
+        d[i] = new Dashboard_1;
+        t->attach(d[0]);
+    }
     return 0;
-}
+} 
